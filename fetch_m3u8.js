f27819ac3cd83e30aa58c7178ee0eb1b85c1397f
pratik karbhal <62989313+pratikkarbhal@users.noreply.github.com>
2024-10-21 13:11:33 +0530

    Update fetch_m3u8.js

--- fetch_m3u8.js
+++ fetch_m3u8.js
@@ -8,7 +8,7 @@ const puppeteer = require('puppeteer');
     const page = await browser.newPage();
     
     // Define the URL of the page you want to scrape
-    const targetUrl = 'YOUR_TARGET_URL_HERE'; // Replace with the actual URL
+    const targetUrl = 'https://www.shemaroome.com/all-channels/shemaroo-marathibana'; // Replace with the actual URL
     await page.goto(targetUrl, { waitUntil: 'networkidle2' });
 
     // Extract the M3U8 URL
